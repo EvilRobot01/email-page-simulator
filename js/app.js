22ceac1e7aae5c58afe303a885fb37ec3d72a203
Harold DRF <95robots@gmail.com>
2019-02-13 23:59:50 -0700

    Form validation

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 //Variables
 const sendBtn = document.getElementById('sendBtn');
       email = document.getElementById('email');
-      subject = document.getElementById('subjet');
-
+      subject = document.getElementById('subject');
+      message = document.getElementById('message');
 
 //Event listeners
 addEventListeners();
 
 function addEventListeners(){
-    //Ap init
+    //App init
     document.addEventListener('DOMContentLoaded', appInit);
+
+    //Validate the forms
+    email = document.addEventListener('blur', validateForm);
+    subject = document.addEventListener('blur', validateForm);
+    message = document.addEventListener('blur', validateForm);
 }
 
 //Functions
@@ -18,4 +23,41 @@ function addEventListeners(){
 //App Initialization
 function appInit(){
 sendBtn.disabled = true;
+}
+
+//Validate Form
+function validateForm(){
+    let errors;
+
+    //Validate the length of the forms\
+    validateLength(this);
+
+    //Validate the email
+    if(this.type==='email'){
+        validateEmail(this);
+    }
+    
+}
+
+//Validate the forms' lengths
+function validateLength(field){
+    if(field.value.length > 0){
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    }else{
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
+
+//Validate the email
+function validateEmail(field){
+    let emailText = field.value;
+    if(emailText.indexOf('@') !== -1){
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    }else{
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
 }
